c6589827b88be3cae3fa5cbbf72b4260994024fd
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 23:01:07 +0800

    Add files via upload

---
+++ basick function string class/basick function string class/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  basick function string class
+//
+//  Created by Nisha Ramprasath on 2/11/23.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string str="qwert";
+    cout<<str.length()<<endl;
+    cout<<str<<endl;
+    return 0;
+}
